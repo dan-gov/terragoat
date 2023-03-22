c03f2510bb2de1ce13f3dd55d55266a944ec7166
Dan Schtechel <62330792+b4ttlesh33p@users.noreply.github.com>
2023-03-22 07:44:18 +0200

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web_ho11st" {
+resource "aws_instance" "web_ho111st" {
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
   instance_type = "t2.nano"
